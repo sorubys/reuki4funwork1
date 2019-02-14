b8a15a51aa04f73a8f7a7ca15d62c9ac75ab91b9
sorubys <soruby1298@gamil.com>
2019-02-14 09:48:28 +0530

    fun

---
+++ js/main.js
@@ -0,0 +1,198 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+    SC.stream('/tracks/498231915',function(sound){
+         $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/29589286',function(sound){
+         $('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/357656207',function(sound){
+         $('#start2').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop2').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/34552851',function(sound){
+         $('#start3').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop3').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/642894183',function(sound){
+         $('#start4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/529850505',function(sound){
+         $('#start5').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop5').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/309607440',function(sound){
+         $('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/296037065',function(sound){
+         $('#start7').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop7').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/510823947',function(sound){
+         $('#start8').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop8').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/501548730',function(sound){
+         $('#start9').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop9').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/345819962',function(sound){
+         $('#start10').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop10').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/293511153',function(sound){
+         $('#start11').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop11').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/371193194',function(sound){
+         $('#start12').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop12').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+
+    SC.stream('/tracks/583634289',function(sound){
+         $('#start13').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop13').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/162505654',function(sound){
+         $('#start14').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop14').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+
+     SC.stream('/tracks/396283209',function(sound){
+              $('#start15').click(function(e) {
+                e.preventDefault();
+                sound.start();
+              });
+
+              $('#stop15').click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+         });
+    });
